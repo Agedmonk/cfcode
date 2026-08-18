c21b9ed6426344a421b54b8ad82221341ca00502
Agedmonk <laijunling@gmail.com>
2026-08-18 14:39:00 +0800

    增加检查新版提示升级功能

--- install.js
+++ install.js
@@ -1,4 +1,4 @@
-const CURRENT_VERSION = "1.0.202608181430"; // 当前版本号，置于顶部方便随时修改
+const CURRENT_VERSION = "1.0.202608181438"; // 当前版本号，置于顶部方便随时修改
 
 export default {
   async fetch(request, env, ctx) {
